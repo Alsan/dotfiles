df41d9cfbe03516f569e0f21fb9ccc6a264a8029
alsan <alsan.wong@gmail.com>
2023-06-16 07:32:56 +0800

    add xdg_state_home env var

--- .config/env.d/01.env.zsh
+++ .config/env.d/01.env.zsh
@@ -1,4 +1,5 @@
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_STATE_HOME=$XDG_CONFIG_HOME
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_SCREENSHOTS_DIR="$HOME/Pictures/5.screenshoots"
