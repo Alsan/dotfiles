dbfdcf04ee211b0f6dd630963330b8d0d9e9f2ce
alsan <alsan.wong@gmail.com>
2023-06-27 09:34:25 +0800

    better handle of project list

--- bin/open-vscode-project.sh
+++ bin/open-vscode-project.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# WORKSPACE_LIST="$(plocate -d ~/Documents/files.index code-workspace | grep -Ev '\.cache|\.local|node_module|History|Template' | sed "s|$HOME/||g" | sort)"
-# SELECTED="$(echo "$WORKSPACE_LIST" | wofi -d -sep ' ' -i -G -p 'vscode project')"
-
-SELECTED=$(wofi -d -sep ' ' -i -G -p 'vscode project' <$XDG_CACHE_HOME/vscode-project-list)
+PROJ_LIST=$(cat $XDG_CACHE_HOME/vscode-project-list | sort | uniq -c | sort -nr | awk '{print $2}')
+SELECTED=$(wofi -d -sep ' ' -i -G -p 'vscode project' <<<$PROJ_LIST)
 
 [[ -z "$SELECTED" ]] && exit 1
 
-JAVA_HOME=$SDKMAN_CANDIDATES_DIR/java/current \
-OPENSSL_CONF=$XDG_CONFIG_HOME/openssl.cnf \
-code \
-  --new-window \
-  --enable-proposed-api GitHub.copilot \
-  --proxy-server="socks5://127.0.0.1:1080" \
-  "$HOME/$SELECTED"
+echo "$SELECTED" >> $CACHE_LIST
+$HOME/bin/code "$HOME/$SELECTED"
